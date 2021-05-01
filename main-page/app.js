ec66442db3d3766274f7f74ab651b1c93a05219f
Jittojoyes98 <51293782+Jittojoyes98@users.noreply.github.com>
2021-05-01 17:08:49 +0530

    Initial commit

---
+++ main-page/app.js
@@ -0,0 +1,14 @@
+//  image of checking https://iknowfirst.com/wp-content/uploads/2014/11/research1.png
+//  image of machine learning https://iknowfirst.com/wp-content/uploads/2014/11/algo.png
+//  persons image https://iknowfirst.com/wp-content/uploads/2014/11/rpiv.png
+// building     https://iknowfirst.com/wp-content/uploads/2015/12/inst.png
+
+
+// https://miro.medium.com/max/2093/1*okJsgoJDXQD5Nvyu-waDIw.png   - graph1
+// https://miro.medium.com/max/6250/1*MAgqFZnhWemDyzXLXEUUTw.jpeg  - graph2
+// https://miro.medium.com/max/1280/1*dSbEKnRVhfCJMgLfQDtrCw.jpeg  -graph3
+// https://makeshiftinsights.com/wp-content/uploads/2019/02/article_predictions_rs56vs.png  -graph4
+
+
+// external  https://miro.medium.com/max/645/1*deV4OgAFVhdobwPKHWIeLQ.png
+// external  https://miro.medium.com/max/3383/1*hnJmoDkR6-inqCe_JRxW0w.png
